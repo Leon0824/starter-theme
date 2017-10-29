807c5a386485a317706e7fabd5a57a721664e940
Leon <leon0824@gmail.com>
2017-10-29 18:02:12 +0800

    I4: add background color

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,21 +1,27 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-SECRET_NUMBER = rand(10000)
+SECRET_NUMBER = rand(100)
+$background_color = ""
 
 def check_guess(guess)
   guess = guess.to_i
   if guess == 0
     # 
   elsif guess > SECRET_NUMBER * 5
+    $background_color = "red"
     "Way too high!"
   elsif guess > SECRET_NUMBER
+    $background_color = "mistyrose"
     "Too high!"
   elsif guess * 5 < SECRET_NUMBER
+    $background_color = "red"
     "Way too low!"
   elsif guess < SECRET_NUMBER
+    $background_color = "mistyrose"
     "Too low!"
   else
+    $background_color = "green"
     "The SECRET NUMBER is #{SECRET_NUMBER}"
   end
 end
@@ -23,5 +29,5 @@ end
 get '/' do
   guess = params["guess"]
   message = check_guess(guess)
-  erb :index, :locals => {:message => message}
+  erb :index, :locals => {:message => message, :background_color => $background_color}
 end
